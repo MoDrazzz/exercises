a3faea0bc14c08573d2f68506d674e8349e5a3b9
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-02-08 15:49:18 +0100

    feat: add items filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { Searchbox } from './components'
 
+const colors = [
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'orange',
+  'purple',
+  'pink',
+  'cyan',
+  'magenta',
+  'teal',
+]
 function App() {
   return (
     <div className="grid place-items-center text-xl bg-neutral-800 w-full h-screen">
-      <Searchbox />
+      <Searchbox items={colors} placeholder="Search..." />
     </div>
   )
 }
--- src/components/Searchbox.tsx
+++ src/components/Searchbox.tsx
@@ -1,7 +1,45 @@
-export const Searchbox = () => {
+import { useEffect, useState } from 'react'
+
+type Props = {
+  items: string[]
+  placeholder: string
+}
+
+export const Searchbox = ({ items, placeholder }: Props) => {
+  const [isActive, setIsActive] = useState(false)
+  const [query, setQuery] = useState('')
+  const [filteredItems, setFilteredItems] = useState(items)
+
+  useEffect(() => {
+    const newFilteredItems = items.filter((item) =>
+      item.toLowerCase().startsWith(query.toLowerCase()),
+    )
+
+    setFilteredItems(newFilteredItems)
+  }, [query, items])
+
   return (
-    <div>
-      <input />
+    <div className="relative text-neutral-100">
+      <input
+        className="w-96 py-2 px-3 rounded-lg outline-none border-neutral-500 focus:border-neutral-400 transition-colors bg-neutral-900 border-2"
+        placeholder={placeholder}
+        onFocus={() => setIsActive(true)}
+        onBlur={() => setIsActive(false)}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {isActive && (
+        <ul className="absolute top-full mt-1 bg-neutral-900 border-2 rounded-lg w-full max-h-40 overflow-auto p-1 border-neutral-400">
+          {filteredItems.map((item, index) => (
+            <li
+              key={index}
+              className="py-2 px-3 capitalize rounded-lg cursor-pointer hover:bg-neutral-800"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
