b5c564ccbdc00555eff9fdf0e82f59f44a99f8f8
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-02-09 13:42:04 +0100

    feat: read keyboard keys excluding modifiers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [, setWrittenKonami] = useState('')
+
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      setWrittenKonami((prev) => (e.key.length > 1 ? prev : prev + e.key))
+    }
+
+    window.addEventListener('keydown', handleKeyPress)
+
+    return () => window.removeEventListener('keydown', handleKeyPress)
+  }, [])
+
   return (
     <div className="w-full h-screen bg-neutral-800 text-slate-50 flex justify-center items-center gap-10 flex-col">
       <header>
