371dcf8f8e421bea34ae6a850054682030fd8671
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-02-09 13:23:08 +0100

    feat: setup initial view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <h1 className="text-red-500">Hello world</h1>
+  return (
+    <div className="w-full h-screen bg-neutral-800 text-slate-50 flex justify-center items-center gap-10 flex-col">
+      <header>
+        <h1 className="text-3xl font-bold">Sweet Kittens!</h1>
+      </header>
+      <img src="http://placekitten.com/200/300" alt="Sweet cat" />
+    </div>
+  )
 }
 
 export default App
